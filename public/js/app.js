44f73eb084b07a1d4d6c5d367f2ccbaf87a41901
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-07 13:50:49 +0200

    done with exercise 2, used setAttribute

--- public/js/app.js
+++ public/js/app.js
@@ -15,10 +15,14 @@ req.onload = () => {
 
     const elementH1 = document.createElement('h1');
     const elementP = document.createElement('p');
+    const elementImg = document.createElement('img')
 
     elementP.textContent = data.intro;
     elementH1.textContent = data.headline;
+    elementImg.setAttribute('src', data.articleImage);
 
     document.getElementById('Exercise1').appendChild(elementH1);
     document.getElementById('Exercise1').appendChild(elementP);
+    document.getElementById('Exercise1').appendChild(elementImg);
+
 };
