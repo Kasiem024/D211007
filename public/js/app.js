533bdf4e1850b4ed789a46668ad49c65d6bdbcf2
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-07 10:31:46 +0200

    testing with mdn example

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
 'use strict';
 
 console.log('app.js is alive!')
+
+let dataURL = '../data/exercise1.json';
+let req = new XMLHttpRequest();
+req.open('GET', dataURL);
+req.responseType = 'json';
+req.send();
+
+console.log('data received');
+
+req.onload = () => {
+
+    document.getElementById('Exercise1').innerHTML;
+
+    const list = document.getElementById("list");
+
+    list.innerHTML += `<li><a href="#">Item ${list.children.length + 1}</a></li>`;
+
+};
