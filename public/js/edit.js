4b8839de02737ebdf59f0a3584304d869ef563c7
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-07 15:16:06 +0200

    exercise 4 done

--- public/js/edit.js
+++ public/js/edit.js
@@ -17,13 +17,23 @@ req.onload = () => {
     const tBox1 = document.createElement('input');
     const tBox2 = document.createElement('input');
     const tBox3 = document.createElement('input');
+    const btnUpdate = document.createElement('button');
+
+    document.getElementById('Exercise2').style.maxWidth = '180px';
 
     tBox1.value = data.headline;
     tBox2.value = data.intro;
     tBox3.value = data.articleImage;
+    btnUpdate.textContent = 'Update';
+
+    btnUpdate.addEventListener('click', ButtonEventhandler);
 
     document.getElementById('Exercise2').appendChild(tBox1);
     document.getElementById('Exercise2').appendChild(tBox2);
     document.getElementById('Exercise2').appendChild(tBox3);
-
+    document.getElementById('Exercise2').appendChild(btnUpdate);
 };
+
+const ButtonEventhandler = () => {
+    console.log('Button Update pushed')
+}
