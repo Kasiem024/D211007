90ddc3562d57c8de114d9c7ed813bc99616f7b94
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-08 14:28:45 +0200

    gave textboxes names, created form testing editUpdate

--- controller/homeController.js
+++ controller/homeController.js
@@ -7,6 +7,15 @@ exports.index = (req, res) => {
 exports.edit = (req, res) => {
     res.sendfile('public/edit.html');
 };
+
 exports.editUpdate = (req, res) => {
-    console.log('editUpdate is alive!')
+    console.log('editUpdate is alive!');
+
+    console.log("data:" + req.body.tBoxName1);
+    console.log("data:" + req.body.tBoxName2);
+    console.log("data:" + req.body.tBoxName3);
+
+    console.log('editUpdate is alive again!');
+    res.sendfile('./public/edit.html');
+
 };
--- public/js/edit.js
+++ public/js/edit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('edit.js is alive!')
+console.log('edit.js is alive!');
 
 let dataURL = '../data/exercise1.json';
 let req = new XMLHttpRequest();
@@ -10,19 +10,26 @@ req.send();
 
 console.log('Data received');
 
-
 req.onload = () => {
     const data = req.response;
 
+    const form = document.createElement('form')
     const tBox1 = document.createElement('input');
     const tBox2 = document.createElement('input');
     const tBox3 = document.createElement('input');
     const btnUpdate = document.createElement('button');
 
-    tBox1.id = 'tBoxid1'
-    tBox2.id = 'tBoxid2'
-    tBox3.id = 'tBoxid3'
-    btnUpdate.id = 'btnUpdateId'
+    tBox1.id = 'tBoxId1';
+    tBox2.id = 'tBoxId2';
+    tBox3.id = 'tBoxId3';
+    tBox1.name = 'tBoxName1';
+    tBox2.name = 'tBoxName2';
+    tBox3.name = 'tBoxName3';
+    btnUpdate.id = 'btnUpdateId';
+    form.id = 'formId';
+
+    form.action = '/edit';
+    form.method = 'POST';
 
     document.getElementById('Exercise2').style.maxWidth = '180px';
 
@@ -33,10 +40,12 @@ req.onload = () => {
 
     btnUpdate.addEventListener('click', ButtonEventhandler);
 
-    document.getElementById('Exercise2').appendChild(tBox1);
-    document.getElementById('Exercise2').appendChild(tBox2);
-    document.getElementById('Exercise2').appendChild(tBox3);
-    document.getElementById('Exercise2').appendChild(btnUpdate);
+
+    document.getElementById('Exercise2').appendChild(form);
+    document.getElementById('formId').appendChild(tBox1);
+    document.getElementById('formId').appendChild(tBox2);
+    document.getElementById('formId').appendChild(tBox3);
+    document.getElementById('formId').appendChild(btnUpdate);
 };
 
 const ButtonEventhandler = () => {
@@ -45,9 +54,5 @@ const ButtonEventhandler = () => {
     const tBox3 = document.getElementById('tBoxId3');
     const btnUpdate = document.getElementById('btnUpdateId');
 
-    tBox1.value = data.headline;
-    tBox2.value = data.intro;
-    tBox3.value = data.articleImage;
-
-    console.log('Button Update pushed')
+    console.log('Button Update pushed');
 }
