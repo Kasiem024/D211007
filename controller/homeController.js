976ef77012a8e4b34e7fce51a00f2ce6941377e2
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-08 14:53:45 +0200

    done with exercise 5

--- controller/homeController.js
+++ controller/homeController.js
@@ -11,11 +11,21 @@ exports.edit = (req, res) => {
 exports.editUpdate = (req, res) => {
     console.log('editUpdate is alive!');
 
-    console.log("data:" + req.body.tBoxName1);
-    console.log("data:" + req.body.tBoxName2);
-    console.log("data:" + req.body.tBoxName3);
+    const fs = require('fs');
+
+    const headline = req.body.tBoxName1;
+    const intro = req.body.tBoxName2;
+    const articleImage = req.body.tBoxName3;
+
+    const test = { headline, intro, articleImage };
+
+    const testAnka = JSON.stringify(test);
+
+    fs.writeFile('./public/data/exercise1.json', testAnka, function(err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
 
     console.log('editUpdate is alive again!');
     res.sendfile('./public/edit.html');
-
 };
